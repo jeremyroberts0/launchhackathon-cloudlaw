5c2d6363c45ab0930a5c8544c98263de73910aaa
Jeremy Roberts <jeremyroberts0@gmail.com>
2013-03-02 20:40:33 -0800

    More stuff

---
+++ templates/header.template.js
@@ -0,0 +1,21 @@
+$.notification().listen('initialize', 'header', '*', function(notification) {
+	var id = notification.getId();
+	
+	$.notification().listen('updateTemplate', 'header', id, function(notification) {
+		var payload = notification.getPayload();
+		getPrimaryUser(function(credentials){
+			if (credentials === undefined) {
+				$('#'+id+' .usernameLink').hide();
+				$('#'+id+' .loginLink').show();
+			} else if (credentials.userId !== undefined) {
+				$('#'+id+' .loginLink').html(credentials.userId);
+				$('#'+id+' .loginLink').hide();
+				$('#'+id+' .usernameLink').show();
+			}
+		});
+	});
+	
+
+	$.notification().notify('updateTemplate', 'header', id, { });
+});
+
