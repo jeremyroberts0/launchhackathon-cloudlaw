758158d4c58b3f95b1129306a37f56c5fa32118a
Jeremy Roberts <jeremyroberts0@gmail.com>
2013-03-03 23:14:50 -0800

    Finished documents section on book page and CSS touch-ups throughout

--- templates/packages.template.js
+++ templates/packages.template.js
@@ -1,6 +1,18 @@
-
-
 $.notification().listen('initialize', 'packages', '*', function(notification) {
+	var id = notification.getId();
 	
-}
+	$('#'+id+' .documentsLink').click(function() {
+		var hidden = $(this).data('hidden');
+		var parent = $(this).parent().parent();
+		
+		if (hidden === undefined || hidden === true) {
+			parent.children('.documentsList').slideDown();
+			$(this).data('hidden', false);
+		} else if (hidden === false) {
+			parent.children('.documentsList').slideUp();
+			$(this).data('hidden', true);
+		}
+	});
+	
+});
 
