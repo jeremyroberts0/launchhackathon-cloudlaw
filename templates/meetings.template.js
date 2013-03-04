78976ac47f80233cfc8789cb4303e0334e1bd809
Jeremy Roberts <jeremyroberts0@gmail.com>
2013-03-04 13:48:50 -0800

    Added checks to stop account page inits from firing twice

--- templates/meetings.template.js
+++ templates/meetings.template.js
@@ -3,22 +3,29 @@
 $.notification().listen('initialize', 'meetings', '*', function(notification) {
 	var id = notification.getId();
 	
-	$('.meeting-card').each(function(){
-		var time = $('a', this).attr('href');
-		time = time.replace('meeting.html?id=', '');
-		time = parseInt(time);
+	var fired = notification.getData('fired', false);
+	if (fired === false) {
+		$('.meeting-card').each(function(){
+			var time = $('a', this).attr('href');
+			time = time.replace('meeting.html?id=', '');
+			time = parseInt(time);
+			
+			var date = new Date();
+			var currentTime = date.getTime();
+//			if (time + 3600000 < currentTime) {
+//				$('a', this).removeAttr('href').html('Meeting Over').addClass('meeting-unavailable');
+//			} else {
+////				if (time - 900000 < currentTime && time + 3600000 < currentTime) {
+//					$('a', this).html('Start Meeting');
+////				}
+//			}
+			
+		});
 		
-		var date = new Date();
-		var currentTime = date.getTime();
-//		if (time + 3600000 < currentTime) {
-//			$('a', this).removeAttr('href').html('Meeting Over').addClass('meeting-unavailable');
-//		} else {
-////			if (time - 900000 < currentTime && time + 3600000 < currentTime) {
-//				$('a', this).html('Start Meeting');
-////			}
-//		}
-		
-	});
+		notification.setData('fired', true);
+	}
+	
+	
 	
 });
 
--- templates/packages.template.js
+++ templates/packages.template.js
@@ -1,18 +1,22 @@
 $.notification().listen('initialize', 'packages', '*', function(notification) {
 	var id = notification.getId();
+	var fired = notification.getData('fired', false);
+	if (fired === false) {
+		$('#'+id+' .documentsLink').click(function() {
+			var hidden = $(this).data('hidden');
+			var parent = $(this).parent().parent();
+			
+			if (hidden === undefined || hidden === true) {
+				parent.children('.documentsList').slideDown();
+				$(this).data('hidden', false);
+			} else if (hidden === false) {
+				parent.children('.documentsList').slideUp();
+				$(this).data('hidden', true);
+			}
+		});
+		notification.setData('fired', true);
+	}
 	
-	$('#'+id+' .documentsLink').click(function() {
-		var hidden = $(this).data('hidden');
-		var parent = $(this).parent().parent();
-		
-		if (hidden === undefined || hidden === true) {
-			parent.children('.documentsList').slideDown();
-			$(this).data('hidden', false);
-		} else if (hidden === false) {
-			parent.children('.documentsList').slideUp();
-			$(this).data('hidden', true);
-		}
-	});
 	
 });
 
